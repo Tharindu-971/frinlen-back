81026290033a942c4d3a5bbc2a9e56e5b73e9e60
Tharindu-971 <htharindu971@gmail.com>
2022-11-16 17:31:54 +0530

    azure

--- src/main/java/com/techscroll/frinlen/Service/Auth/AuthServiceImpl.java
+++ src/main/java/com/techscroll/frinlen/Service/Auth/AuthServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +38,7 @@ public class AuthServiceImpl implements AuthService{
     private RoleRepository roleRepository;
 
     @Override
+    @Transactional
     public ResponseEntity<LoginResponseDto> login(LoginRequestDto loginRequestDto){
         System.out.println("Otu");
         try {
