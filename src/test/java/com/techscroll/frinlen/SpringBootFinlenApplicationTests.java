6412776e4b72e4581f49e5163c612aa06d985a39
Tharindu-971 <htharindu971@gmail.com>
2022-11-16 06:57:13 +0530

    azure

--- src/test/java/com/techscroll/frinlen/SpringBootFinlenApplicationTests.java
+++ src/test/java/com/techscroll/frinlen/SpringBootFinlenApplicationTests.java
@@ -3,7 +3,7 @@ package com.techscroll.frinlen;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootFinlenApplicationTests.class)
 public class SpringBootFinlenApplicationTests {
 
 	@Test
