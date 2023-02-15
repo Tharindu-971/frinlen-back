c63547fd321d0282ce16bb850afe1adc803c95ad
Tharindu Madushan <development@techscrolll.com>
2023-02-15 20:24:36 +0530

    ngrx init commit-pom

--- src/main/java/com/techscroll/frinlen/mapper/product/ProductMapper.java
+++ src/main/java/com/techscroll/frinlen/mapper/product/ProductMapper.java
@@ -1,5 +1,12 @@
 package com.techscroll.frinlen.mapper.product;
 
+import com.techscroll.frinlen.dto.product.ProductResponseDto;
+import com.techscroll.frinlen.entity.product.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ProductMapper {
+
+    ProductResponseDto productToProductResponseDto(Product product);
 
 }
